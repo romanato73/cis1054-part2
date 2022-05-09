aad9ccefa7bcc8d54d2d24c052d9763feafe56ea
romanato73 <romanato.orsz@gmail.com>
2022-05-09 11:52:36 +0200

    Database implementation

---
+++ helpers/Database.php
@@ -0,0 +1,88 @@
+<?php
+
+class Database
+{
+    private static array $table = [];
+
+    private array $result = [];
+
+    /**
+     * Load the table (file) into variable.
+     *
+     * @param string $name
+     * @return static
+     */
+    public static function table(string $name): static
+    {
+        self::$table = json_decode(file_get_contents(__DIR__ . "/../database/$name.json"));
+
+        return new static();
+    }
+
+    /**
+     * Extract data that match the condition.
+     *
+     * @param  string  $column
+     * @param  string  $operator
+     * @param  string  $value
+     * @return $this
+     */
+    public function where(string $column, string $operator, string $value): static
+    {
+        foreach (self::$table as $row) {
+            if ($this->compare($row->$column, $value, $operator)) {
+                $this->result[] = $row;
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get row(s) from table.
+     * If id is not set return all rows
+     * otherwise return one row with specific id.
+     *
+     * @param int|null $id
+     * @return array|mixed|null
+     */
+    public function get(int $id = null): mixed
+    {
+        if (is_null($id)) {
+            if (!empty($this->result)) {
+                return $this->result;
+            }
+
+            return self::$table;
+        }
+
+        foreach (self::$table as $row) {
+            if ($row->id === $id) return $row;
+        }
+
+        die("Row with id '$id' does not exist.");
+    }
+
+    /**
+     * Compare two vales with specified operator.
+     *
+     * @param $val1
+     * @param $val2
+     * @param $op
+     * @return bool
+     */
+    private function compare($val1, $val2, $op): bool
+    {
+        switch ($op) {
+            case '==': return $val1 == $val2;
+            case '>': return $val1 > $val2;
+            case '<': return $val1 < $val2;
+            case '>=': return $val1 >= $val2;
+            case '<=': return $val1 <= $val2;
+            case '!=': return $val1 != $val2;
+            case '===': return $val1 === $val2;
+            case '!==': return $val1 !== $val2;
+            default: return false;
+        }
+    }
+}
