4214177ad66de1e521c0c0bf791a176d92ae2f26
Olesia09 <103413794+Olesia09@users.noreply.github.com>
2022-06-05 19:37:20 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,8 +1,44 @@
 <?php
-
-require_once __DIR__.'/Database.php';
-
-$database = new Database();
-
-$food = $database -> get("SELECT id, name FROM type order by category");
+session_start();
+include_once "helpers/Database.php";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant</title>
+    <link rel="stylesheet" href="/css/favourites.css">
+    <link rel="stylesheet" href="/lib/fontawesome/css/all.min.css">
+</head>
+
+<body>
+        <?php
+        $food = [];
+
+        if (isset($_GET['category'])) {
+            $list = Database::table('dishes')->get();
+
+            foreach ($list as $dish) {
+                if ($dish->category == $_GET['category']) {
+                    $food = $list;
+                }
+            }
+
+            if (empty($food)) {
+                exit("Not found");
+            }
+ 
+        foreach($food as $list){
+            echo $list->name;
+        }
+    }else {
+        exit('Not found');
+    }
+        ?>
+</body>
+
+</html>
